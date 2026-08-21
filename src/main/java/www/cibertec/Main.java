2de2d84c9db9770c0e1909f08c0b451ad6d7bb1e
Walter Salazar <ws120706@gmail.com>
2026-08-20 23:22:42 -0500

    Ejercicio realizado del proyecto final

--- src/main/java/www/cibertec/Main.java
+++ src/main/java/www/cibertec/Main.java
@@ -1,17 +1,53 @@
 package www.cibertec;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        public static double calcularPrecioPostre ( int tipo){
+            double precio = 0.0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+            if (tipo == 1) {
+                precio = 6.0;  // Tres leches
+            } else if (tipo == 2) {
+                precio = 8.0;  // Cheesecake
+            } else {
+                precio = 10.0; // Torta de chocolate
+            }
+
+            return precio;
+        }
+
+        public static void main (String[]args){
+
+            String cliente = "Manuel";
+            int cantidadPorciones = 4, opcionPostre = 3;
+            double precioUnitario, totalAcumulado = 0.0;
+
+            String nombrePostre = "";
+
+            switch (opcionPostre) {
+                case 1:
+                    nombrePostre = "Tres Leches";
+                    break;
+                case 2:
+                    nombrePostre = "Cheesecake";
+                    break;
+                case 3:
+                    nombrePostre = "Torta de Chocolate";
+                    break;
+            }
+            precioUnitario = calcularPrecioPostre(opcionPostre);
+
+            for (int i = 1; i <= cantidadPorciones; i++) {
+                totalAcumulado = totalAcumulado + precioUnitario;
+            }
+
+            // Salida de resultados
+            System.out.println("TICKET DE CAFETERÍA");
+            System.out.println("Cliente: " + cliente);
+            System.out.println("Postre: " + nombrePostre);
+            System.out.println("Cantidad: " + cantidadPorciones);
+            System.out.println("TOTAL A PAGAR: S/. " + totalAcumulado);
+
+
         }
     }
-}
